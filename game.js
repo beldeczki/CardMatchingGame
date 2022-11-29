893255117daaee3755365507d678d55991f4695e
Boldoczki Patrik <h848398@stud.u-szeged.hu>
2022-11-29 20:00:39 +0100

    Header, set paired cards' opacity, start new game function

--- game.js
+++ game.js
@@ -1,43 +1,60 @@
 const cardsImgs = ["angular.png", "d3.png", "evista.png", "jenkins.png", "postcss.png", "react.png", "redux.png", "sass.png", "ts.png", "webpack.png"]
 
-let cards = 20
-let cardsPlaced = new Array(cards)
 let gameArea = document.getElementById("gameArea")
+let deckSize = document.getElementById("deckSize")
+let cards = null
+let cardsPlaced = null
 let cardImg = null
-let cardsImgsCopy = [...cardsImgs];
-
-for(let i = 0; i < cards; i++) {
-    let cardImg_i = null
-
-    if(i % 2 === 0) {
-        do {
-            cardImg_i = Math.floor(Math.random() * cardsImgsCopy.length)
-        } while(cardsImgsCopy[cardImg_i] == null)
-
-        cardImg = cardsImgsCopy[cardImg_i]
-        cardsImgsCopy[cardImg_i] = null
-    }
-
-    let slot = null
-
-    do {
-        slot = Math.floor(Math.random() * cards)
-    } while(cardsPlaced[slot] != undefined)
-
-    cardsPlaced[slot] = cardImg
-}
-
-for(let i = 0; i < cards; i++) {
-    let card = "<div class=\"col\"><div onclick=\"flipCard(" + i + ")\" class=\"card text-center h-100\" style=\"width: 7rem;\"><div class=\"card-body\"><img id=\"" + i + "\" src=\"cards/" + cardsPlaced[i] + "\" class=\"card-img-top\" style=\"width: 5rem;\" hidden></div></div></div>"
-    gameArea.innerHTML += card
-
-    if(i + 1 === cards)
-        break;
-}
-
+let cardsImgsCopy = null
 let cardFlipped_first_i = null
 let cardFlipped_second_i = null
 let secondFlipInProgress = false
+let playerTries = 0
+let hideTimeout = null
+
+function newGame() {
+    cards = Number(deckSize.value)
+    cardsPlaced = new Array(cards)
+    cardImg = null
+    cardsImgsCopy = [...cardsImgs];
+    cardFlipped_first_i = null
+    cardFlipped_second_i = null
+    secondFlipInProgress = false
+    playerTries = 0
+
+    clearTimeout(hideTimeout)
+
+    for(let i = 0; i < cards; i++) {
+        let cardImg_i = null
+    
+        if(i % 2 === 0) {
+            do {
+                cardImg_i = Math.floor(Math.random() * cardsImgsCopy.length)
+            } while(cardsImgsCopy[cardImg_i] == null)
+    
+            cardImg = cardsImgsCopy[cardImg_i]
+            cardsImgsCopy[cardImg_i] = null
+        }
+    
+        let slot = null
+    
+        do {
+            slot = Math.floor(Math.random() * cards)
+        } while(cardsPlaced[slot] != undefined)
+    
+        cardsPlaced[slot] = cardImg
+    }
+
+    gameArea.innerHTML = ""
+    
+    for(let i = 0; i < cards; i++) {
+        let card = "<div class=\"col\"><div onclick=\"flipCard(" + i + ")\" class=\"card text-center\" style=\"width: 7rem; height: 7rem;\"><div class=\"card-body\"><img id=\"" + i + "\" src=\"cards/" + cardsPlaced[i] + "\" class=\"card-img-top\" hidden></div></div></div>"
+        gameArea.innerHTML += card
+    
+        if(i + 1 === cards)
+            break;
+    }
+}
 
 function flipCard(i) {
     if(cardsPlaced[i] == null || secondFlipInProgress)
@@ -60,26 +77,31 @@ function flipCard(i) {
         secondFlipInProgress = true
     }
 
+    playerTries++
+    console.log(playerTries)
+
+    let firstCard = document.getElementById(cardFlipped_first_i)
+    let secondCard = document.getElementById(cardFlipped_second_i)
+
     if(cardsPlaced[cardFlipped_first_i] === cardsPlaced[cardFlipped_second_i]) {
         cardsPlaced[cardFlipped_first_i] = null
         cardsPlaced[cardFlipped_second_i] = null
 
         secondFlipInProgress = false
-        cardFlipped_first_i = null
-        cardFlipped_second_i = null
 
         console.log("new pair")
+
+        firstCard.setAttribute("style", "opacity: 0.5;")
+        secondCard.setAttribute("style", "opacity: 0.5;")
     } else {
-        setTimeout(function() {
-            card = document.getElementById(cardFlipped_first_i)
-            card.setAttribute("hidden", "true")
-            card = document.getElementById(cardFlipped_second_i)
-            card.setAttribute("hidden", "true")
+        hideTimeout = setTimeout(function() {
+            firstCard.setAttribute("hidden", "true")
+            secondCard.setAttribute("hidden", "true")
 
             secondFlipInProgress = false
-            cardFlipped_first_i = null
-            cardFlipped_second_i = null
         }, 1000)
-
     }
+
+    cardFlipped_first_i = null
+    cardFlipped_second_i = null
 }
